096c5c1f5dbc8a03ae640a9e8c58c7004dfd4c90
yu <13112433432@163.com>
2022-06-22 15:20:27 +0800

    第一版本想法

--- register_test.go
+++ register_test.go
@@ -1,24 +1,27 @@
 package ginex
 
 import (
-	"github.com/Merry74751/ginex/common"
 	"github.com/Merry74751/ginex/middleware"
 	"github.com/Merry74751/ginex/router"
 	"github.com/gin-gonic/gin"
-	"log"
-	"reflect"
 	"testing"
 )
 
-func TestType(t *testing.T) {
-	v := common.Res{}
-	ty := reflect.TypeOf(v)
-	t.Log(ty.PkgPath())
+type Test struct {
+	getById any `router:"path:/getById,params:id,method:GET"`
 }
 
-func TestRegister(t *testing.T) {
-	app := gin.Default()
+func (t Test) GetById(c *gin.Context, id string) {
+	c.JSON(200, gin.H{
+		"message": "请求成功",
+		"data":    id,
+	})
+}
+
+func TestRegisterRouter(t *testing.T) {
 	router.AddRouter(Test{})
+	app := gin.Default()
+
 	router.RegisterRouter(app)
 
 	err := app.Run()
@@ -27,56 +30,7 @@ func TestRegister(t *testing.T) {
 	}
 }
 
-func Register2(app *gin.Engine) {
-	test := Test{}
-	v := reflect.ValueOf(test)
-	method := v.MethodByName("Insert")
-	typ := method.Type().In(0)
-	value := reflect.New(typ)
-	bv := value.Interface()
+func TestName(t *testing.T) {
+	middleware.Logger.Info("INFO")
 
-	app.POST("/test", func(c *gin.Context) {
-		err := c.BindJSON(bv)
-		if err != nil {
-			log.Println("bind error:", err)
-		}
-		bvv := reflect.ValueOf(bv).Elem()
-		values := method.Call([]reflect.Value{bvv})
-		values[0].Interface().(gin.HandlerFunc)(c)
-	})
-}
-
-func TestRegister2(t *testing.T) {
-	app := gin.Default()
-	Register2(app)
-
-	err := app.Run()
-	if err != nil {
-		t.Log(err)
-	}
-}
-
-func TestLog(t *testing.T) {
-	middleware.Logger.Info("error")
-}
-
-type Test struct {
-	Name string `json:"name,omitempty"`
-	Age  int    `json:"age,omitempty"`
-}
-
-func (receiver Test) GroupName() string {
-	return "test"
-}
-
-func (receiver Test) Test() (string, string, router.Handle) {
-	return "GET", "getById", func(c *gin.Context) any {
-		return "hello"
-	}
-}
-
-func (t Test) Insert(test Test) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		c.JSONP(200, test)
-	}
 }
--- router/register.go
+++ router/register.go
@@ -1,55 +1,91 @@
 package router
 
 import (
-	"github.com/Merry74751/ginex/common"
 	"github.com/Merry74751/yutool/anyutil"
 	"github.com/gin-gonic/gin"
+	"log"
 	"reflect"
 	"strings"
 )
 
-type Handle func(c *gin.Context) any
-
-type Group interface {
-	GroupName() string
-}
-
 var router = make([]any, 0)
 
-func AddRouter(r any) {
-	router = append(router, r)
+func AddRouter(v any) {
+	if anyutil.IsStruct(v) {
+		router = append(router, v)
+	}
 }
 
 func RegisterRouter(app *gin.Engine) {
+	if len(router) == 0 {
+		return
+	}
 	for _, route := range router {
-		var routerGroup *gin.RouterGroup
-		group, ok := route.(Group)
-		if ok {
-			routerGroup = app.Group(group.GroupName())
-		}
+		v := anyutil.Value(route)
+		typ := anyutil.Type(route)
 
-		methods := anyutil.Methods(route)
-		for _, method := range methods {
-			if isHandle(method) {
-				results := method.Call(nil)
-				httpMethod := results[0].Interface().(string)
-				path := results[1].Interface().(string)
-				h := results[2].Interface().(Handle)
-				if ok {
-					routerGroup.Handle(httpMethod, path, common.ConvertHandle(h))
-					continue
-				}
-				app.Handle(httpMethod, path, common.ConvertHandle(h))
+		numMethod := v.NumMethod()
+		for i := 0; i < numMethod; i++ {
+			m := v.Method(i)
+			mty := typ.Method(i)
+
+			mName := mty.Name
+			mName = firstToLower(mName)
+
+			field, exist := typ.FieldByName(mName)
+			if !exist {
+				log.Printf("%T are not have field: %s", v, mName)
 			}
+			tag := field.Tag.Get("router")
+			tags := strings.Split(tag, ",")
+
+			var (
+				path   string
+				params []string
+				method string
+			)
+
+			for _, t := range tags {
+				kv := strings.Split(t, ":")
+				k := kv[0]
+				v := kv[1]
+				if k == "path" {
+					path = v
+				} else if k == "method" {
+					method = v
+				} else if k == "params" {
+					vs := strings.Split(v, ",")
+					params = make([]string, len(vs))
+					for i, v := range vs {
+						params[i] = v
+					}
+				}
+			}
+
+			vs := make([]reflect.Value, len(params)+1)
+			if method == "GET" {
+				app.Handle(method, path, func(c *gin.Context) {
+					vs[0] = reflect.ValueOf(c)
+					for i, param := range params {
+						q := c.Query(param)
+						vs[i+1] = anyutil.Value(q)
+					}
+					m.Call(vs)
+				})
+				return
+			}
+			// todo post、delete、put请求处理，处理路径变量
+			// 新思路 用结构体保存 各个参数：
+			// 1、请求路径
+			// 2、请求方式
+			// 3、路径变量
+			// 4、请求体或GET参数或POST表单参数
 		}
 	}
 }
 
-func isHandle(method reflect.Value) bool {
-	mt := method.Type().String()
-	s := strings.SplitN(mt, " ", 2)[1]
-	if s == "(string, string, router.Handle)" {
-		return true
-	}
-	return false
+func firstToLower(s string) string {
+	bytes := []byte(s)
+	bytes[0] = bytes[0] + 32
+	return string(bytes)
 }
